26be2f4aa37c5d98c7e325a5f1bb574e148c4037
miriamformaro <formaromiriam@gmail.com>
2026-01-19 11:19:15 +0100

    Create Esercitazione11-12-2025Haskell.hs

---
+++ Esercitazione11-12-2025Haskell.hs
@@ -0,0 +1,6 @@
+-- Definire in Haskell una funzione che riceve una lista di coppie di numeri interi e restituisce una lista di liste in cui ogni lista corripsondente ad una coppia (x,k) contiene la sequenza [x, 2*x, ..., k*x]
+-- convertiLista:: [(Int,Int)] -> [[Int]] 
+-- Esempio
+-- convertilista [(1,3), (2,2)] = [[1,2,3],[2,4]]
+convL :: [(Int,Int)] -> [[Int]]
+convL xs = [ [x*i | i<-[1..k]] | (x,k)<-xs]
