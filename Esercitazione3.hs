0db0704591e6a137023aadf32840c29b3db0c309
miriamformaro <formaromiriam@gmail.com>
2026-01-21 10:16:57 +0100

    Update Esercitazione3.hs

--- Esercitazione3.hs
+++ Esercitazione3.hs
@@ -4,3 +4,22 @@
 -- removeElementsByIndices [1,4,2,1] [2,1] restituisce [4]
 removeIndices :: [Int] -> [Int] -> [Int]
 removeIndices xs ys = [x | x<-xs, x `notElem` ys]
+
+-- Scrivi una funzione che prende due liste e restituisce una lista contenente gli elementi della prima che sono presenti anche nella seconda. L'ordine deve essere quello della prima lista.
+-- keepCommon [1, 4, 2, 5, 1] [2, 1, 9]
+keepCommon :: [Int] -> [Int] -> [Int]
+keepCommon [] _ = []
+keepCommon _ [] = []
+keepCommon xs ys = [x | x<-xs, x `elem` ys]
+
+-- Scrivi una funzione che prende una lista di valori e una lista di indici (interi). Devi rimuovere dalla prima lista gli elementi che si trovano alle posizioni specificate nella seconda lista. (Assumiamo indici base-0).
+-- removeByIndices [10, 20, 30, 40, 50] [0, 2]
+removeByIndices :: [Int] -> [Int] -> [Int]
+removeByIndices [] _ = []
+removeByIndices _ [] = []
+removeByIndices xs ys = [x | (i,x)<-zip[0..]xs, i `notElem` ys]
+
+-- Prendi una lista di valori e una lista di Booleani della stessa lunghezza. Tieni l'elemento solo se il booleano corrispondente è True.
+-- filterByMask [1, 2, 3, 4] [True, False, False, True]
+filterByMask :: [Int] -> [Bool] -> [Int]
+filterByMask xs ys = [x | (i,x)<-zip[0..]xs, (j,y)<-zip[0..]ys, i==j, y==True]
