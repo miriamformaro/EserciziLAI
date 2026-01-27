85ea31e2056acb4e1f31a74dffff8a799eb95041
miriamformaro <formaromiriam@gmail.com>
2026-01-27 12:15:53 +0100

    Create Grafi.hs

---
+++ Grafi.hs
@@ -0,0 +1,67 @@
+cricca :: [Node] -> Graph -> Bool
+cricca n g = all verifica n
+    where
+        verifica :: Node -> Bool
+        verifica v = case Map.lookup v g of
+            Nothing -> False
+            Just vs ->
+                let nodi = filter (/=v) n
+                in all (\v -> v `elem` vs) nodi
+
+isIndipendent :: [Node] -> Graph -> Bool
+isIndipendent n g = all verifica n
+    where 
+        verifica :: Node -> Bool
+        verifica v = case Map.lookup v g of
+            Nothing -> False
+            Just vs ->
+                let nodi = filter (/=v) n
+                in all (\v -> v `notElem` vs) nodi
+
+esisteNodo :: Node -> Graph -> Bool
+esisteNodo v g = case Map.lookup v g of
+                    Nothing -> False
+                    Just x -> True
+
+quantiVicini :: Node -> Graph -> Int
+quantiVicini v g = case Map.lookup v g of
+                    Nothing -> 0
+                    Just vs -> length vs
+
+hasEdge :: Node -> Node -> Graph -> Bool
+hasEdge s e g = case Map.lookup s g of
+                    Nothing -> False
+                    Just vs -> e `elem` vs
+
+haCappio :: Node -> Graph -> Bool
+haCappio n g = case Map.lookup n g of
+                    Nothing -> False
+                    Just vs -> n `elem` vs
+
+amiciVeri :: Node -> Node -> Graph -> Bool
+amiciVeri a b g =
+    let 
+        ab =case Map.lookup a g of
+                    Nothing -> False
+                    Just vs -> b `elem` vs
+        ba = case Map.lookup b g of
+                    Nothing -> False
+                    Just vs -> a `elem` vs
+    in
+        ab && ba
+
+tuttiPuntanoA :: Node -> [Node] -> Graph -> Bool
+tuttiPuntanoA v n g = all verifica n
+    where
+        verifica :: Node -> Bool
+        verifica x = case Map.lookup x g of
+                        Nothing -> False
+                        Just vs -> v `elem` vs
+
+nodiConKVicini :: Int -> [Node] -> Graph -> [Node]
+nodiConKVicini k n g = filter verifica n
+    where
+        verifica :: Node -> Bool
+        verifica x = case Map.lookup x g of
+                        Nothing -> False
+                        Just vs -> length vs == k
