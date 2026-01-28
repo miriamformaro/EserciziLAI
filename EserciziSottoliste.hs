68bddbc78cebc1b8232202ec1bb518542aadac7f
miriamformaro <formaromiriam@gmail.com>
2026-01-28 19:35:11 +0100

    Update EserciziSottoliste.hs

--- EserciziSottoliste.hs
+++ EserciziSottoliste.hs
@@ -8,3 +8,9 @@ sumSub x s = filter(\subS -> sum subS == x) (sub s)
 
 cardinalita :: Int -> [Int] -> [[Int]]
 cardinalita x s = filter(\l -> length l == x) (sub s)
+
+- consecutivi
+consecutivi :: [Int] -> [[Int]]
+consecutivi [] = [[]]
+consecutivi [x] = [[x]]
+consecutivi (x:y:xs) = if y == x+1 then (x : head (consecutivi (y:xs))) : tail (consecutivi (y:xs)) else [x] : consecutivi(y:xs)
