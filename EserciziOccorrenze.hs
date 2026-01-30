b1c190c07bf17cba10b8c22f3d95490a406a20b8
miriamformaro <formaromiriam@gmail.com>
2026-01-30 16:42:50 +0100

    Update EserciziOccorrenze.hs

--- EserciziOccorrenze.hs
+++ EserciziOccorrenze.hs
@@ -22,3 +22,30 @@ deleteF k (x:xs) = if k == x then xs else x : deleteF k xs
 deleteL :: Int -> [Int] -> [Int]
 deleteL _ [] = []
 deleteL k (x:xs) = if x==k && k `notElem` xs then xs else x : deleteL k xs
+
+removeFirst :: Int -> [Int] -> [Int]
+removeFirst _ [] = []
+removeFirst y (x:xs) = if y==x then xs else x : removeFirst y xs
+
+countY :: Int -> [Int] -> Int
+countY _ [] = 0
+countY y (x:xs) = if x==y then 1 + countY y xs else countY y xs
+
+keepFirst :: Int -> [Int] -> [Int]
+keepFirst _ [] = []
+keepFirst y (x:xs) = if y==x then x : filter (/=y) xs else x : keepFirst y xs
+
+compress :: [Int] -> [Int]
+compress [] = []
+compress [x] = [x]
+compress (x:y:xs) = if x==y then compress (y:xs) else x : compress (y:xs)
+
+onlyUnique :: [Int] -> [Int]
+onlyUnique xs = [x | x<-xs, countY x xs == 1]
+
+onlyR :: [Int] -> [Int]
+onlyR xs = [x | x<-xs, countY x xs /= 1]
+
+removeLast :: Int -> [Int] -> [Int]
+removeLast _ [] = []
+removeLast y (x:xs) = if x==y && y `notElem` xs then xs else x : removeLast y xs
